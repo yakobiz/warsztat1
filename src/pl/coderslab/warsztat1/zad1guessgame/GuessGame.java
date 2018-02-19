6c3f2c8fdf02fea67448539dee3ce5656c7bd323
kuba <yakobi@o2.pl>
2018-02-19 21:56:14 +0100

    GuessGame -first version working

--- src/pl/coderslab/warsztat1/zad1guessgame/GuessGame.java
+++ src/pl/coderslab/warsztat1/zad1guessgame/GuessGame.java
@@ -1,35 +1,43 @@
 package pl.coderslab.warsztat1.zad1guessgame;
 
-import java.util.Scanner;
-import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessGame {
 
 	public static void main(String[] args) {
 
+		
+		
+		
 		askUser();
+
 	}
 
 	static void askUser() {
-
+		
 		Random r = new Random();
-		int a = r.nextInt(3);
-try {
+		int draw = r.nextInt(101);
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Zgadnij jaka to liczba?\n");
-		int b = sc.nextInt(); } catch (InputMismatchException e){
-			System.out.println("Wyjątek" + e.getMessage());
+		System.out.println("Zgadnij liczbę: ");
+		int guess = sc.nextInt();
+
+		while (draw != guess) {
+			if (draw < guess) {
+				System.out.println("Za dużo!"+"\n");
+				guess = sc.nextInt();
+			}
+
+			else if (draw > guess) {
+				System.out.println("Za mało!"+"\n");
+				guess = sc.nextInt();
+			}
+		}
+		if (draw == guess) {
+			System.out.println("Zgadłeś!!!");
+			sc.close();
 		}
 
-		while (true) {
-			a /= b;
-			System.out.println("Zgaduj jeszcze raz \n");
-			sc.nextInt();
-			sc.close();
-		} 
-			
-
-		
 	}
 }
