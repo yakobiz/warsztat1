15db690fffd25cacd553c7ee4f2b07655c7c6b3c
kuba <yakobi@o2.pl>
2018-02-19 22:52:47 +0100

    second ver working

--- src/pl/coderslab/warsztat1/zad1guessgame/GuessGame.java
+++ src/pl/coderslab/warsztat1/zad1guessgame/GuessGame.java
@@ -3,41 +3,44 @@ package pl.coderslab.warsztat1.zad1guessgame;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class GuessGame {
 
 	public static void main(String[] args) {
 
 		
-		
-		
 		askUser();
-
 	}
 
 	static void askUser() {
-		
+
 		Random r = new Random();
 		int draw = r.nextInt(101);
-
+		int guess;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Zgadnij liczbę: ");
-		int guess = sc.nextInt();
+		try {
+			guess = sc.nextInt();
 
-		while (draw != guess) {
-			if (draw < guess) {
-				System.out.println("Za dużo!"+"\n");
-				guess = sc.nextInt();
+			while (draw != guess) {
+				if (draw < guess) {
+					System.out.println("Za dużo!" + "\n");
+					System.out.print("Zgadnij liczbę: ");
+					guess = sc.nextInt();
+				}
+
+				else if (draw > guess) {
+					System.out.println("Za mało!");
+					System.out.print("Zgadnij liczbę: ");
+					guess = sc.nextInt();
+				}
 			}
-
-			else if (draw > guess) {
-				System.out.println("Za mało!"+"\n");
-				guess = sc.nextInt();
+			if (draw == guess) {
+				System.out.println("Zgadłeś!!!");
+				sc.close();
 			}
+		} catch (java.util.InputMismatchException e) {
+			System.out.println("To nie jest liczba!! " + e.getMessage());	
 		}
-		if (draw == guess) {
-			System.out.println("Zgadłeś!!!");
-			sc.close();
-		}
-
 	}
 }
